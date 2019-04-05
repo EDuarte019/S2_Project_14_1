548498d0df09341575cea68b8d3d91f44c52a25b
Evelyn Duarte <eduarte019@west-mec.org>
2019-04-05 16:30:28 -0700

    Final Commit
    Added a css style. Ready for review.

--- na_styler.js
+++ na_styler.js
@@ -20,9 +20,11 @@
       Returns a random integer from 0 up to size-1.
 
 */
+//The function of setStyles will load when the page does. 
 window.addEventListener("load", setStyles);
 
 function setStyles() {
+      // variables are just created to store code under a single name. I also set attributes to stylesheets and by placing a stylesheet, you must append the varaible and name. 
       var styleNum = randInt(5);
       var linkE = document.createElement("link");
       linkE.setAttribute("rel", "stylesheet");
@@ -31,11 +33,12 @@ function setStyles() {
 
       document.head.appendChild(linkE);
 
+
       var figBox = document.createElement("figure");
       figBox.setAttribute("id", "styleThumbs");
-
+      figBox.setAttribute("name", "figBox");
       document.getElementById("box").appendChild(figBox);
-
+      // For loop that has the index value ranging from 0 to 4. Within, an element is created along with the name of the style sheets that will be different images depending on their number, specified by i.
       for (var i = 0; i <= 4; i++) {
             var sheetIMG = document.createElement("img");
             sheetIMG.setAttribute("src", "na_small_" + i + ".png");
@@ -49,24 +52,25 @@ function setStyles() {
       }
       var thumbStyles = document.createElement("style");
       document.head.appendChild(thumbStyles);
-
+      //css code put into text strings using the insertRule.
       document.styleSheets[document.styleSheets.length - 1].insertRule(
             "figure#styleThumbs {\
                   position: absolute;\
-                  left: 0 px;\
-                  bottom: 0 px;\
+                  left: 0px;\
+                  bottom: 0px;\
             }"
       )
       document.styleSheets[document.styleSheets.length - 1].insertRule(
             "figure#styleThumbs img {\
             outline: 1px solid black;\
-            opacity:0.75;\
+            cursor: pointer;\
+            opacity: 0.75;\
             } "
       )
       document.styleSheets[document.styleSheets.length - 1].insertRule(
-            "figure#styleThumbs img:hover{\
+            "figure#styleThumbs img:hover {\
             outline: 1px solid red;\
-            opacity:1.0;\
+            opacity: 1.0;\
             }"
       )
 
